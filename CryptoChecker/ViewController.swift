9268f918803a57050d57cb25a2538895cbf03946
ash-old <ashley.oldham@gmail.com>
2021-07-05 23:02:47 +0100

    Initial Commit

---
+++ CryptoChecker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoChecker
+//
+//  Created by Ash Oldham on 05/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
