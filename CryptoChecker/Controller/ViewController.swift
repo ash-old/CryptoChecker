0e33d5714455a20935a29c136f7fee0b8ac61cb2
ash-old <ashley.oldham@gmail.com>
2021-07-07 00:03:34 +0100

    adopted 2 protocols as extensions. Created IBOutlets and assigned to appropriate labels and picker

--- CryptoChecker/Controller/ViewController.swift
+++ CryptoChecker/Controller/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  let coinManager = CoinManager()
 
+  @IBOutlet weak var cryptoPicker: UIPickerView!
+  @IBOutlet weak var currencyLabelUSD: UILabel!
+  @IBOutlet weak var coinImage: UIImageView!
+  @IBOutlet weak var currencyLabelGBP: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    cryptoPicker.dataSource = self
+    cryptoPicker.delegate = self
   }
 
+}
 
+//MARK: - UIPickerViewDataSource
+
+extension ViewController: UIPickerViewDataSource {
+  
+  func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    return 1
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return coinManager.cryptoArray.count
+  }
+}
+
+//MARK: - UIPickerViewDelegate
+
+extension ViewController: UIPickerViewDelegate {
+  
+  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    return coinManager.cryptoArray[row]
+  }
 }
 
---
+++ CryptoChecker/Model/CoinManager.swift
@@ -0,0 +1,17 @@
+//
+//  CoinManager.swift
+//  CryptoChecker
+//
+//  Created by Ash Oldham on 06/07/2021.
+//
+
+import Foundation
+
+struct CoinManager {
+  
+  let url = "https://rest.coinapi.io/v1/exchangerate/BTC"
+  let apiKey = ""
+  
+  let cryptoArray = ["1", "2", "3", "4"]
+  
+}
